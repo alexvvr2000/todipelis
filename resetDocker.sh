44df4293b2cd0e422249e656496bc3c3cbfb3497
Alejandro Valenzuela Rivera <alejandrovalenzuela051@gmail.com>
2024-05-11 23:39:13 -0600

    implementado resetDocker con lista de volumenes

--- resetDocker.sh
+++ resetDocker.sh
@@ -1,1 +1,34 @@
 #!/bin/bash
+
+contenedores=("baseTodiPelis")
+imagenes=("todipelis_base")
+volumenes=("folderDatosTodiPelis")
+
+for contenedor in "${contenedores[@]}"; do
+    if docker rm -v "${contenedor}" >/dev/null 2>&1; then
+        echo "Contenedor ${contenedor} ha sido borrada"
+    else
+        echo "No se borro el contenedor ${contenedor}"
+    fi
+done
+
+for imagen in "${imagenes[@]}"; do
+    if docker rmi "${imagen}" >/dev/null 2>&1; then
+        echo "Imagen ${imagen} ha sido borrado"
+    else
+        echo "No se borro la imagen ${imagen}"
+    fi
+done
+
+for volumen in "${volumenes[@]}"; do
+    if docker volume rm "${volumen}" >/dev/null 2>&1; then
+        echo "Volumen ${volumen} ha sido eliminado"
+    else
+        echo "No se borro el volumen ${volumen}"
+    fi
+done
+
+sudo rm -rf ./mariadb_data
+
+docker compose build --no-cache &&
+    docker compose up
