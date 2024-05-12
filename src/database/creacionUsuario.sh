89c6bfe1048865190d72d1602d0d4f3daf2dbf94
Alejandro Valenzuela Rivera <alejandrovalenzuela051@gmail.com>
2024-05-12 01:35:24 -0600

    ahora se crea usuario dinamicamente con sh

--- src/database/creacionUsuario.sh
+++ src/database/creacionUsuario.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+
+tablasBases=("Usuario" "Pelicula" "ListaCriticas" "ListaFavoritos")
+
+echo "CREATE USER 'api'@'%' IDENTIFIED BY '${API_PASSWORD_FILE}';"
+
+for tabla in "${tablasBases[@]}"; do
+    echo "GRANT INSERT, UPDATE, DELETE, SELECT ON todipelis.${tabla} TO 'api'@'%';"
+    echo "GRANT EXECUTE ON ${tabla}.* TO 'api'@'%' IDENTIFIED BY '${API_PASSWORD_FILE}';"
+done
