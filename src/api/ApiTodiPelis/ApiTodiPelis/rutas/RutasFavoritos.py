53566c1c76a4d734be26649e49c0a15c522421f9
Alejandro Valenzuela Rivera <alejandrovalenzuela051@gmail.com>
2024-05-19 11:45:09 -0600

    agregada ruta para obtener favoritos

--- src/api/ApiTodiPelis/ApiTodiPelis/rutas/RutasFavoritos.py
+++ src/api/ApiTodiPelis/ApiTodiPelis/rutas/RutasFavoritos.py
@@ -1,3 +1,20 @@
 from flask import Blueprint, jsonify
+from ApiTodiPelis.conexion import obtenerConexion
+from ApiTodiPelis.operaciones.Favoritos import obtenerFavoritos
+from ApiTodiPelis.types import ListaFavoritos
+from mariadb import Connection
+from typing import List
 
 rutasFavoritosBlueprint: Blueprint = Blueprint("rutasFavoritos", __name__)
+
+
+@rutasFavoritosBlueprint.route("/1/favoritos/", methods=["GET"])
+def obtenerFavoritosUsuario():
+    conexion: Connection = obtenerConexion()
+    favoritosUsuario: List[ListaFavoritos] = obtenerFavoritos(conexion)
+    return jsonify(
+        {
+            "cantidadFavoritos": len(favoritosUsuario),
+            "favoritosUsuario": favoritosUsuario,
+        }
+    )
