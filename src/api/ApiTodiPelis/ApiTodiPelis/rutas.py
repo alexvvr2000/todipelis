44493da25bf0e7670653de046b51e17e7dbdf28e
Alejandro Valenzuela Rivera <alejandrovalenzuela051@gmail.com>
2024-05-19 09:34:22 -0600

    agregado error handler para error 404

--- src/api/ApiTodiPelis/ApiTodiPelis/rutas.py
+++ src/api/ApiTodiPelis/ApiTodiPelis/rutas.py
@@ -1,6 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, abort, jsonify
 from ApiTodiPelis.operaciones.select import obtenerPelicula
 from ApiTodiPelis.conexion import obtenerConexion
+from ApiTodiPelis.types import Pelicula
 from mariadb import Connection
 from dataclasses import asdict
 
@@ -10,5 +11,12 @@ rutas: Blueprint = Blueprint("rutas", __name__)
 @rutas.route("/pelicula/<string:idPelicula>", methods=["GET"])
 def ruta(idPelicula: str):
     conexion: Connection = obtenerConexion()
-    peliculaBase = obtenerPelicula(conexion, idPelicula)
+    peliculaBase: Pelicula | None = obtenerPelicula(conexion, idPelicula)
+    if peliculaBase is None:
+        abort(404, description="Pelicula no existe en base")
     return asdict(peliculaBase)
+
+
+@rutas.errorhandler(404)
+def peliculaNoEncontrada(error):
+    return jsonify(error=str(error)), 404
