69785203f275948872152461396c5b32077b97ce
Alejandro Valenzuela Rivera <alejandrovalenzuela051@gmail.com>
2024-05-19 12:48:33 -0600

    agregada ruta para obtener datos de usuario

---
+++ src/api/ApiTodiPelis/ApiTodiPelis/operaciones/Usuario.py
@@ -0,0 +1,13 @@
+from mariadb import Connection, Cursor
+from ApiTodiPelis.types import Usuario
+
+
+def obtenerDatosUsuarioBase(conexion: Connection) -> Usuario:
+    cursor: Cursor = conexion.cursor()
+    cursor.callproc("procedureUsuario", (1,))
+    usuarioRetornado = cursor.fetchone()
+    return Usuario(
+        idUsuario=1,
+        nombreUsuario=usuarioRetornado[0],
+        urlFotoPerfil=usuarioRetornado[1] if usuarioRetornado[1] is not None else None,
+    )
--- src/api/ApiTodiPelis/ApiTodiPelis/rutas/RutasUsuario.py
+++ src/api/ApiTodiPelis/ApiTodiPelis/rutas/RutasUsuario.py
@@ -1,3 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from ApiTodiPelis.conexion import obtenerConexion
+from ApiTodiPelis.types import Usuario
+from ApiTodiPelis.operaciones.Usuario import obtenerDatosUsuarioBase
+from mariadb import Connection
 
 rutasUsuarioBlueprint: Blueprint = Blueprint("rutasUsuario", __name__)
+
+
+@rutasUsuarioBlueprint.route("/1", methods=["GET"])
+def obtenerDatosUsuario():
+    conexion: Connection = obtenerConexion()
+    usuarioBase: Usuario = obtenerDatosUsuarioBase(conexion)
+    return jsonify(usuarioBase)
