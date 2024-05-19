56fa97a2cac57b56296ae26ea43646f9e6ed6a9f
Alejandro Valenzuela Rivera <alejandrovalenzuela051@gmail.com>
2024-05-18 23:47:01 -0600

    eliminada funcion existeUsuario

--- src/api/ApiTodiPelis/ApiTodiPelis/operaciones/select.py
+++ src/api/ApiTodiPelis/ApiTodiPelis/operaciones/select.py
@@ -7,14 +7,6 @@ from ApiTodiPelis.ApiTodiPelis.types import (
 )
 
 
-def existeUsuario(conexion: Connection, idUsuario: int) -> bool:
-    cursor: Cursor = conexion.cursor()
-    cursor.execute("select funcionUsuarioExiste(?) as existe", (idUsuario,))
-    filaRetornada = cursor.fetchone()
-    usuarioExiste: bool = filaRetornada[0] == 1
-    return usuarioExiste
-
-
 def existePelicula(conexion: Connection, idPelicula: str) -> bool:
     cursor: Cursor = conexion.cursor()
     cursor.execute("select funcionPeliculaExiste(?), as existe", (idPelicula,))
