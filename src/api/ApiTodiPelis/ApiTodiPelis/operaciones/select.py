78d17e3dc3c534c4a0cf4636fe77d4de612bd375
Alejandro Valenzuela Rivera <alejandrovalenzuela051@gmail.com>
2024-05-19 01:38:46 -0600

    agregada forma de obtener peliculas de omdb si no existe en base

--- src/api/ApiTodiPelis/ApiTodiPelis/operaciones/select.py
+++ src/api/ApiTodiPelis/ApiTodiPelis/operaciones/select.py
@@ -1,4 +1,10 @@
+from typing import Dict
+from flask import g
+from requests import get, Response
+from get_docker_secret import get_docker_secret
 from mariadb import Connection, Cursor
+from ApiTodiPelis.operaciones.insert import agregarPeliculaBase
+from ApiTodiPelis.conexion import existePeliculaBase, obtenerConexion
 from ApiTodiPelis.types import (
     Pelicula,
     ListaCriticas,
@@ -7,24 +13,17 @@ from ApiTodiPelis.types import (
 )
 
 
-def existePeliculaBase(conexion: Connection, idPelicula: str) -> bool:
-    cursor: Cursor = conexion.cursor()
-    cursor.execute("select funcionPeliculaExiste(?) as existe", (idPelicula,))
-    filaRetornada = cursor.fetchone()
-    peliculaExiste: bool = filaRetornada[0] == 1
-    return peliculaExiste
-
-
-def obtenerPelicula(conexion: Connection, idPelicula: str) -> Pelicula | None:
+def obtenerPelicula(idPelicula: str) -> Pelicula | None:
     peliculaBase: Pelicula | None
-    if existePeliculaBase(conexion, idPelicula):
+    conexion: Connection = obtenerConexion()
+    if existePeliculaBase(idPelicula):
         cursor: Cursor = conexion.cursor()
         cursor.callproc("procedureObtenerPelicula", (idPelicula,))
         filaRetornada = cursor.fetchone()
-        print(filaRetornada)
+        cursor.close()
         peliculaBase: Pelicula = Pelicula(
             idPelicula=idPelicula,
-            titulo=[0],
+            titulo=filaRetornada[0],
             genero=filaRetornada[1],
             urlPoster=filaRetornada[2],
             rating=filaRetornada[3],
@@ -32,5 +31,24 @@ def obtenerPelicula(conexion: Connection, idPelicula: str) -> Pelicula | None:
         )
         return peliculaBase
     else:
-        peliculaBase = None
+        urlBase: str = "http://www.omdbapi.com/"
+        parametros: Dict[str, str] = {
+            "apikey": get_docker_secret("api-key"),
+            "i": idPelicula,
+        }
+        respuesta: Response = get(urlBase, params=parametros)
+        if respuesta.status_code != 200:
+            return None
+        datosPeliculas = respuesta.json()
+        if datosPeliculas.get("Response") == "False":
+            return None
+        peliculaBase = Pelicula(
+            idPelicula=idPelicula,
+            titulo=datosPeliculas.get("Title"),
+            genero=datosPeliculas.get("Genre"),
+            urlPoster=datosPeliculas.get("Poster"),
+            rating=datosPeliculas.get("imdbRating"),
+            sinopsis=datosPeliculas.get("Plot"),
+        )
+        agregarPeliculaBase(peliculaBase)
     return peliculaBase
