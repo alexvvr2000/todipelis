e319c051bc60407d781d76e3193881d958bb79b4
Alejandro Valenzuela Rivera <alejandrovalenzuela051@gmail.com>
2024-05-19 01:37:20 -0600

    cambiado archivo conexion a base de libreria

--- src/api/ApiTodiPelis/ApiTodiPelis/conexion.py
+++ src/api/ApiTodiPelis/ApiTodiPelis/conexion.py
@@ -1,6 +1,15 @@
 from get_docker_secret import get_docker_secret
-from mariadb import connect, Connection
+from mariadb import connect, Connection, Cursor
 from flask import g
+
+
+def existePeliculaBase(idPelicula: str) -> bool:
+    cursor: Cursor = obtenerConexion().cursor()
+    cursor.execute("select funcionPeliculaExiste(?) as existe", (idPelicula,))
+    filaRetornada = cursor.fetchone()
+    peliculaExiste: bool = filaRetornada[0] == 1
+    cursor.close()
+    return peliculaExiste
 
 
 def obtenerConexion() -> Connection:
