42a90424149a9e237dc06170552e1fb1d6c90852
Alejandro Valenzuela Rivera <alejandrovalenzuela051@gmail.com>
2024-05-18 23:19:58 -0600

    agregada funcion para obtener conexion a la base de datos

---
+++ src/api/ApiTodiPelis/ApiTodiPelis/operaciones/conexion.py
@@ -0,0 +1,15 @@
+from get_docker_secret import get_docker_secret
+from mariadb import connect, Connection
+from flask import g
+
+
+def obtenerConexion() -> Connection:
+    if "conexion" not in g:
+        g.conexion = connect(
+            host="database",
+            port=3306,
+            user="api",
+            password=get_docker_secret("api-password"),
+            database="todipelis",
+        )
+    return g.conexion
