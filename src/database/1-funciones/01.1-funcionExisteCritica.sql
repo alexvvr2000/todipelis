ee4cfbed2d61ccb09bdc8346b243763ea6fb81af
Alejandro Valenzuela Rivera <alejandrovalenzuela051@gmail.com>
2024-05-19 14:23:59 -0600

    creada funcion sql para revisar si critica existe

---
+++ src/database/1-funciones/01.1-funcionExisteCritica.sql
@@ -0,0 +1,19 @@
+use todipelis;
+
+DELIMITER //
+
+CREATE FUNCTION funcionExisteCritica(buscadoIdPelicula VARCHAR(100), buscadoIdUsuario INT)
+RETURNS BOOLEAN
+NOT DETERMINISTIC
+BEGIN
+    DECLARE existe INT;
+
+    SELECT EXISTS(
+        SELECT 1 FROM ListaCriticas
+        WHERE idUsuario = buscadoIdUsuario AND idPelicula = buscadoIdPelicula
+    ) INTO existe;
+
+    RETURN existe
+END//
+
+DELIMITER ;
