e72bd78acfc4b6e29b47594831763969aee14c84
Alejandro Valenzuela Rivera <alejandrovalenzuela051@gmail.com>
2024-05-19 11:32:20 -0600

    agregada funcion para obtener lista de  favoritos en base

--- src/api/ApiTodiPelis/ApiTodiPelis/operaciones/Favoritos.py
+++ src/api/ApiTodiPelis/ApiTodiPelis/operaciones/Favoritos.py
@@ -1,5 +1,6 @@
 from mariadb import Connection, Cursor
-from ApiTodiPelis.types import IdUsuarioPelicula
+from ApiTodiPelis.types import ListaFavoritos, IdUsuarioPelicula
+from typing import List, Tuple
 
 
 def cantidadFavoritosUsuario(conexion: Connection) -> int:
@@ -7,3 +8,17 @@ def cantidadFavoritosUsuario(conexion: Connection) -> int:
     cursor.execute("SELECT funcionCantidadFavoritosUsuario(1) as cantidad")
     valorRetornado = cursor.fetchone()
     return int(valorRetornado[0])
+
+
+def obtenerFavoritos(conexion: Connection) -> List[ListaFavoritos]:
+    cursor: Cursor = conexion.cursor()
+    cursor.callproc("procedureObtenerFavoritos", (1,))
+
+    valoresBase: List[ListaFavoritos] = []
+    favoritoActual = cursor.fetchone()
+    while favoritoActual is not None:
+        valoresBase.append(
+            ListaFavoritos(idFavorito=IdUsuarioPelicula(1, favoritoActual[0]))
+        )
+        favoritoActual = cursor.fetchone()
+    return valoresBase
