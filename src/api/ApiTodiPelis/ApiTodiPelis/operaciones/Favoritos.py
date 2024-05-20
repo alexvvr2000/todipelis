4309a324216dac12822658c6549435d48915a078
Alejandro Valenzuela Rivera <alejandrovalenzuela051@gmail.com>
2024-05-19 20:41:46 -0600

    implementado metodo para agregar favoritos a usuario en base

--- src/api/ApiTodiPelis/ApiTodiPelis/operaciones/Favoritos.py
+++ src/api/ApiTodiPelis/ApiTodiPelis/operaciones/Favoritos.py
@@ -1,6 +1,15 @@
 from mariadb import Connection, Cursor
-from ApiTodiPelis.types import ListaFavoritos, IdUsuarioPelicula
-from ApiTodiPelis.operaciones.Pelicula import existePeliculaBase
+from ApiTodiPelis.types import (
+    ListaFavoritos,
+    IdUsuarioPelicula,
+    Pelicula,
+)
+from ApiTodiPelis.operaciones.Pelicula import (
+    existePeliculaBase,
+    existePeliculaApi,
+    obtenerPelicula,
+    agregarPeliculaBase,
+)
 from typing import List
 
 
@@ -33,3 +42,16 @@ def obtenerFavoritos(conexion: Connection) -> List[ListaFavoritos]:
         )
         favoritoActual = cursor.fetchone()
     return valoresBase
+
+
+def agregarFavoritoBase(conexion: Connection, idPelicula: str) -> IdUsuarioPelicula:
+    if not existePeliculaApi(conexion, idPelicula):
+        raise Exception("Pelicula no existe en base")
+    if not existePeliculaBase(conexion, idPelicula):
+        peliculaNueva: Pelicula = obtenerPelicula(conexion, idPelicula)
+        agregarPeliculaBase(peliculaNueva)
+    cursor: Cursor = conexion.cursor()
+    cursor.callproc("procedureInsertFavoritoUsuario", [1, idPelicula])
+    filaRetornada = cursor.fetchone()
+    cursor.close()
+    return IdUsuarioPelicula(filaRetornada[0], filaRetornada[1])
