15a5e1f6c925717f6849db0381028837142b246b
Alejandro Valenzuela Rivera <alejandrovalenzuela051@gmail.com>
2024-05-19 20:55:09 -0600

    implementado metodo para agregar favoritos a la lista de usuario

--- src/api/ApiTodiPelis/ApiTodiPelis/rutas/RutasFavoritos.py
+++ src/api/ApiTodiPelis/ApiTodiPelis/rutas/RutasFavoritos.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request, abort
 from ApiTodiPelis.conexion import obtenerConexion
-from ApiTodiPelis.operaciones.Favoritos import obtenerFavoritos
-from ApiTodiPelis.types import ListaFavoritos
+from ApiTodiPelis.operaciones.Favoritos import obtenerFavoritos, agregarFavoritoBase
+from ApiTodiPelis.types import ListaFavoritos, IdUsuarioPelicula
 from mariadb import Connection
 from typing import List
 
@@ -16,5 +16,22 @@ def obtenerFavoritosUsuario():
         {
             "cantidadFavoritos": len(favoritosUsuario),
             "favoritosUsuario": favoritosUsuario,
+        }
+    )
+
+
+@rutasFavoritosBlueprint.route("/1/favoritos", methods=["POST"])
+def agregarFavoritoUsuario():
+    conexion: Connection = obtenerConexion()
+    idPeliculaAgregada: str = request.args.get("idPelicula", "", type=str)
+    if idPeliculaAgregada == "":
+        abort(404)
+    nuevoFavorito: IdUsuarioPelicula = agregarFavoritoBase(conexion, idPeliculaAgregada)
+    return jsonify(
+        {
+            "idFavorito": {
+                "idPelicula": nuevoFavorito.idPelicula,
+                "idUsuario": nuevoFavorito.idUsuario,
+            }
         }
     )
