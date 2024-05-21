0a7a007ed37c3431ae38dc1bac3fd360efa96422
Alejandro Valenzuela Rivera <alejandrovalenzuela051@gmail.com>
2024-05-20 23:48:06 -0600

    agregado procedimiento base para validar login en mariadb

---
+++ src/database/2-procedureObtener/07.1-procedureLoginValido.sql
@@ -0,0 +1,11 @@
+use todipelis;
+
+DELIMITER //
+
+CREATE PROCEDURE procedureLoginValido(IN correoElectronico VARCHAR(255), claveAccesso VARCHAR(50))
+BEGIN
+    DECLARE idUsuarioRetornado INT;
+    -- codigo
+END//
+
+DELIMITER ;
