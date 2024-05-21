a7eb310d04cd33d46858a941fe208eee251a356e
Alejandro Valenzuela Rivera <alejandrovalenzuela051@gmail.com>
2024-05-20 18:25:00 -0600

    cambiado parametro erroneo en procedure registrar usuario

--- src/database/3-procedureInsert/09.1-procedureRegistrarUsuario.sql
+++ src/database/3-procedureInsert/09.1-procedureRegistrarUsuario.sql
@@ -4,7 +4,7 @@ use todipelis;
 DELIMITER //
 
 CREATE PROCEDURE procedureRegistrarUsuario(
-    IN insertadoIdUsuario INT,
+    IN insertadoClaveAcceso VARCHAR(50),
     IN insertadoCorreoElectronico VARCHAR(100),
 )
 BEGIN
