2bae4d08c82c1f13181804c9648623116f793c5d
Alejandro Valenzuela Rivera <alejandrovalenzuela051@gmail.com>
2024-05-21 01:13:06 -0600

    agregado WWW-Authenticate para rutas sin autorizar

--- src/api/ApiTodiPelis/ApiTodiPelis/app.py
+++ src/api/ApiTodiPelis/ApiTodiPelis/app.py
@@ -1,5 +1,6 @@
 from ApiTodiPelis.ContructorApp import crearApp
-from flask import g
+from werkzeug.exceptions import Unauthorized
+from flask import g, jsonify
 
 aplicacion = crearApp()
 
@@ -9,6 +10,15 @@ def cerrarBase(e=None):
     conexion = g.pop("conexion", None)
     if conexion is not None:
         conexion.close()
+
+
+@aplicacion.errorhandler(Unauthorized)
+def manejar_UsuarioSinAuth(e: Unauthorized):
+    return (
+        jsonify({"msg": str(e)}),
+        401,
+        {"WWW-Authenticate": 'Bearer realm="Login required"'},
+    )
 
 
 if __name__ == "__main__":
