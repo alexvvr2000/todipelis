f3c9351fccf65e1dee04cf7fee2fc8345b9f5e48
Alejandro Valenzuela Rivera <alejandrovalenzuela051@gmail.com>
2024-05-20 18:32:28 -0600

    agregados parametros nombre de usuario a registrar usuario

--- src/database/3-procedureInsert/09.1-procedureRegistrarUsuario.sql
+++ src/database/3-procedureInsert/09.1-procedureRegistrarUsuario.sql
@@ -4,8 +4,9 @@ use todipelis;
 DELIMITER //
 
 CREATE PROCEDURE procedureRegistrarUsuario(
-    IN insertadoClaveAcceso VARCHAR(50),
     IN insertadoCorreoElectronico VARCHAR(100),
+    IN insertadoClaveAcceso VARCHAR(50),
+    IN nombreUsuario VARCHAR(100)
 )
 BEGIN
     -- Codigo
