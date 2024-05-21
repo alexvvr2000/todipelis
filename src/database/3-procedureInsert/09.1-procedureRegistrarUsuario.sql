b0560552c3a9d7a4c9f3405279b9fefbc0a57747
Alejandro Valenzuela Rivera <alejandrovalenzuela051@gmail.com>
2024-05-20 18:22:19 -0600

    agregado procedimiento base para registrar usuario

---
+++ src/database/3-procedureInsert/09.1-procedureRegistrarUsuario.sql
@@ -0,0 +1,14 @@
+
+use todipelis;
+
+DELIMITER //
+
+CREATE PROCEDURE procedureRegistrarUsuario(
+    IN insertadoIdUsuario INT,
+    IN insertadoCorreoElectronico VARCHAR(100),
+)
+BEGIN
+    -- Codigo
+END//
+
+DELIMITER ;
