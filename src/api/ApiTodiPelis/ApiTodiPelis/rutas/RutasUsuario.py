c5cf0aae95843eedd84126b9315b01480f2b59b3
Alejandro Valenzuela Rivera <alejandrovalenzuela051@gmail.com>
2024-05-20 19:41:56 -0600

    agregada ruta para para registrar usuario

--- src/api/ApiTodiPelis/ApiTodiPelis/rutas/RutasUsuario.py
+++ src/api/ApiTodiPelis/ApiTodiPelis/rutas/RutasUsuario.py
@@ -1,7 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from ApiTodiPelis.conexion import obtenerConexion
-from ApiTodiPelis.types import Usuario
-from ApiTodiPelis.operaciones.Usuario import obtenerDatosUsuarioBase
+from ApiTodiPelis.operaciones.Usuario import registrarUsuario
 from mariadb import Connection
 
 rutasUsuarioBlueprint: Blueprint = Blueprint("rutasUsuario", __name__)
@@ -10,3 +9,15 @@ rutasUsuarioBlueprint: Blueprint = Blueprint("rutasUsuario", __name__)
 @rutasUsuarioBlueprint.route("/", methods=["GET"])
 def obtenerDatosUsuario():
     return jsonify({"Conexion": "establecida"})
+
+
+@rutasUsuarioBlueprint.route("/registro", methods=["POST"])
+def registrarUsuarioApi():
+    conexion: Connection = obtenerConexion()
+    correoElectronicoNuevo: str = request.args.get("correoElectronico", "")
+    claveAccesoNueva: str = request.args.get("claveAccesso", "")
+    nombreUsuarioNuevo: str = request.args.get("nombreUsuario", "")
+    nuevaId: int = registrarUsuario(
+        conexion, correoElectronicoNuevo, claveAccesoNueva, nombreUsuarioNuevo
+    )
+    return jsonify({"key": "", "idUsuario": nuevaId})
