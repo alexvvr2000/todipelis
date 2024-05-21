40d4d06fd6107a5990bb95963b69896cecd703ba
Alejandro Valenzuela Rivera <alejandrovalenzuela051@gmail.com>
2024-05-20 22:56:33 -0600

    eliminada ruta innecesario de rutasusuario

--- src/api/ApiTodiPelis/ApiTodiPelis/rutas/RutasUsuario.py
+++ src/api/ApiTodiPelis/ApiTodiPelis/rutas/RutasUsuario.py
@@ -2,13 +2,9 @@ from flask import Blueprint, jsonify, request
 from ApiTodiPelis.conexion import obtenerConexion
 from ApiTodiPelis.operaciones.Usuario import registrarUsuario
 from mariadb import Connection
+from flask_jwt_extended import create_access_token
 
 rutasUsuarioBlueprint: Blueprint = Blueprint("rutasUsuario", __name__)
-
-
-@rutasUsuarioBlueprint.route("/", methods=["GET"])
-def obtenerDatosUsuario():
-    return jsonify({"Conexion": "establecida"})
 
 
 @rutasUsuarioBlueprint.route("/registro", methods=["POST"])
@@ -20,4 +16,4 @@ def registrarUsuarioApi():
     nuevaId: int = registrarUsuario(
         conexion, correoElectronicoNuevo, claveAccesoNueva, nombreUsuarioNuevo
     )
-    return jsonify({"key": "", "idUsuario": nuevaId})
+    return jsonify({"key": create_access_token(nuevaId)})
