7626887cb3c7d7a997013e0b81cdf23d9d479084
Alejandro Valenzuela Rivera <alejandrovalenzuela051@gmail.com>
2024-05-21 00:13:33 -0600

    agregado procedure para verificar logins

--- src/database/2-procedureObtener/07.1-procedureLoginValido.sql
+++ src/database/2-procedureObtener/07.1-procedureLoginValido.sql
@@ -2,10 +2,42 @@ use todipelis;
 
 DELIMITER //
 
-CREATE PROCEDURE procedureLoginValido(IN correoElectronico VARCHAR(255), claveAccesso VARCHAR(50))
+CREATE PROCEDURE procedureLoginValido(
+    IN buscadoCorreoElectronico VARCHAR(255), 
+    IN buscadoClaveAccesso VARCHAR(50)
+)
 BEGIN
-    DECLARE idUsuarioRetornado INT;
-    -- codigo
+    DECLARE saltUsuario CHAR(30);
+    DECLARE hashClaveBuscada CHAR(128);
+    DECLARE datosConcuerdan BOOLEAN;
+
+    IF NOT funcionCorreoExiste(buscadoCorreoElectronico) THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'El usuario buscado no existe en base';
+    END IF;
+
+    SET saltUsuario = (
+        SELECT saltClaveAcceso 
+        FROM Usuario 
+        WHERE correoElectronico = buscadoCorreoElectronico 
+        LIMIT 1
+    );
+
+    SET hashClaveBuscada = SHA2(CONCAT(buscadoClaveAccesso, saltUsuario), 512);
+
+    SET datosConcuerdan = EXISTS(
+        SELECT 1 
+        FROM Usuario 
+        WHERE correoElectronico = buscadoCorreoElectronico 
+        AND claveAccesso = hashClaveBuscada
+    );
+
+    IF NOT datosConcuerdan THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Credenciales inválidas';
+    END IF;
+
+    SELECT idUsuario 
+    FROM Usuario 
+    WHERE correoElectronico = buscadoCorreoElectronico;
 END//
 
 DELIMITER ;
