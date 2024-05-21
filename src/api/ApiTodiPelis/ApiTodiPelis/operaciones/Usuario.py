7b73ebfca49b488deff2cbc5c25172d3fee02557
Alejandro Valenzuela Rivera <alejandrovalenzuela051@gmail.com>
2024-05-20 19:21:08 -0600

    implementada funcion existe correo y registrar usuario en base

--- src/api/ApiTodiPelis/ApiTodiPelis/operaciones/Usuario.py
+++ src/api/ApiTodiPelis/ApiTodiPelis/operaciones/Usuario.py
@@ -12,3 +12,33 @@ def obtenerDatosUsuarioBase(conexion: Connection) -> Usuario:
         nombreUsuario=usuarioRetornado[0],
         urlFotoPerfil=usuarioRetornado[1] if usuarioRetornado[1] is not None else None,
     )
+
+
+def existeCorreoRegistradoBase(conexion: Connection, correoElectronico: str) -> bool:
+    cursor: Cursor = conexion.cursor()
+    cursor.execute("select funcionCorreoExiste(?) as existe", (correoElectronico,))
+    filaRetornada = cursor.fetchone()
+    cursor.close()
+    peliculaExiste: bool = filaRetornada[0] == 1
+    return peliculaExiste
+
+
+def registrarUsuario(
+    conexion: Connection, correoElectronico: str, claveNueva: str, nombreUsuario: str
+) -> Usuario:
+    if correoElectronico == "":
+        raise Exception("Correo no puede estar vacio")
+    if existeCorreoRegistradoBase(conexion, correoElectronico):
+        raise Exception("Correo ya existe en base")
+    if claveNueva == "":
+        raise Exception("Clave de acceso no puede estar vacia")
+    if nombreUsuario == "":
+        raise Exception("Nombre de usuario no puede estar vacio")
+    cursor: Cursor = conexion.cursor()
+    # TODO: encriptar conexion mariadb -> python y viceversa
+    cursor.callproc(
+        "procedureRegistrarUsuario", [correoElectronico, claveNueva, nombreUsuario]
+    )
+    nuevoIdUsuario = cursor.fetchone()
+    conexion.commit()
+    return int(nuevoIdUsuario[0])
