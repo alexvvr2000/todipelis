7405c2ee31f606865a3124e9843bf63d044af2e8
Alejandro Valenzuela Rivera <alejandrovalenzuela051@gmail.com>
2024-05-21 00:30:14 -0600

    agregada ruta para hacer login de usuario

--- src/api/ApiTodiPelis/ApiTodiPelis/rutas/RutasUsuario.py
+++ src/api/ApiTodiPelis/ApiTodiPelis/rutas/RutasUsuario.py
@@ -1,13 +1,13 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, abort
 from ApiTodiPelis.conexion import obtenerConexion
-from ApiTodiPelis.operaciones.Usuario import registrarUsuario
+from ApiTodiPelis.operaciones.Usuario import registrarUsuario, identificarUsuario
 from mariadb import Connection
 from flask_jwt_extended import create_access_token
 
 rutasUsuarioBlueprint: Blueprint = Blueprint("rutasUsuario", __name__)
 
 
-@rutasUsuarioBlueprint.route("/registro", methods=["POST"])
+@rutasUsuarioBlueprint.route("/", methods=["POST"])
 def registrarUsuarioApi():
     conexion: Connection = obtenerConexion()
     correoElectronicoNuevo: str = request.args.get("correoElectronico", "")
@@ -17,3 +17,18 @@ def registrarUsuarioApi():
         conexion, correoElectronicoNuevo, claveAccesoNueva, nombreUsuarioNuevo
     )
     return jsonify({"key": create_access_token(nuevaId)})
+
+
+@rutasUsuarioBlueprint.route("/", methods=["GET"])
+def verificarUsuarioBase():
+    conexion: Connection = obtenerConexion()
+    correoElectronicoUsuario: str = request.headers.get("correoElectronico", "")
+    claveAccesoUsuario: str = request.headers.get("claveAcceso", "")
+    if correoElectronicoUsuario == "":
+        abort(500)
+    if claveAccesoUsuario == "":
+        abort(500)
+    idUsuarioBase: int = identificarUsuario(
+        conexion, correoElectronicoUsuario, claveAccesoUsuario
+    )
+    return jsonify({"key": create_access_token(idUsuarioBase)})
