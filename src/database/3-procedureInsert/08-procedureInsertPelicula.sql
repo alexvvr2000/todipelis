850a220dfd3c730f84197f76a36e1c6c6bb5191d
Alejandro Valenzuela Rivera <alejandrovalenzuela051@gmail.com>
2024-05-26 21:39:01 -0600

    eliminados campos null innecesarios de procedimiento insert pelicula

--- src/database/3-procedureInsert/08-procedureInsertPelicula.sql
+++ src/database/3-procedureInsert/08-procedureInsertPelicula.sql
@@ -1,14 +1,14 @@
-use todipelis;
+USE todipelis;
 
 DELIMITER //
 
 CREATE PROCEDURE procedureInsertPelicula(
-    IN insertadoIdPelicula VARCHAR(100) NOT NULL,
-    IN nuevoTitulo VARCHAR(255) NOT NULL,
-    IN nuevoGenero VARCHAR(255) NULL,
-    IN nuevoUrlPoster VARCHAR(255) NULL,
-    IN rating DECIMAL(4,2) NULL,
-    IN sinopsis TEXT NULL
+    IN insertadoIdPelicula VARCHAR(100),
+    IN nuevoTitulo VARCHAR(255),
+    IN nuevoGenero VARCHAR(255),
+    IN nuevoUrlPoster VARCHAR(255),
+    IN rating DECIMAL(4,2),
+    IN sinopsis TEXT
 )
 BEGIN
     IF LENGTH(nuevoTitulo) = 0 THEN
@@ -30,9 +30,11 @@ BEGIN
     IF LENGTH(sinopsis) = 0 AND sinopsis IS NOT NULL THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Error 2600: Pelicula.sinopsis debe tener 1 caracter o más';
     END IF;
+
     IF funcionPeliculaExiste(insertadoIdPelicula) THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Error 2700: Pelicula.idPelicula ya existe en la base de datos';
     END IF;
+
     INSERT INTO Pelicula (idPelicula, titulo, genero, urlPoster, rating, sinopsis)
     VALUES (insertadoIdPelicula, nuevoTitulo, nuevoGenero, nuevoUrlPoster, rating, sinopsis);
 
